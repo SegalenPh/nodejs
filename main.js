1c5b8c69987d1714d8ff51668a3bbd3de38690ab
loukaine <segalen_ph@hotmail.fr>
2018-01-04 17:08:26 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by loukaine on 04/01/18.
+ */
+var http = require('http');
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+
+app.get('/hello', function (req, res) {
+    res.end('Salut');
+});
+
+http.listen(3000, ()=>{
+    console.log("server started");
+
+});
+
